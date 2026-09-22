14efe943d8aabe026f7450dceb0372cd931c4d81
Mr.M1r4 <Mr.M1r4@proton.me>
2026-09-22 10:56:44 -0500

    chore: bootstrap RIVER Club OS

---
+++ next.config.ts
@@ -0,0 +1,3 @@
+import type {NextConfig} from 'next'
+const nextConfig:NextConfig={output:'export',basePath:process.env.NODE_ENV==='production'?'/RIVERVOLLEYBALLCLUB':'',images:{unoptimized:true}}
+export default nextConfig
